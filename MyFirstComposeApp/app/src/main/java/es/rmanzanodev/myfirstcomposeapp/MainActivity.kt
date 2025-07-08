b1377b12e3eaab0a02741cd371de6ac9e197ae08
RafaelManzanoDev <manzanomedinarafael@gmail.com>
2025-07-08 19:08:41 +0200

    Seccion 1 - Introduccion a Compose
    Introduccion a Compose con Material 3 y subida del proyecto

---
+++ MyFirstComposeApp/app/src/main/java/es/rmanzanodev/myfirstcomposeapp/MainActivity.kt
@@ -0,0 +1,40 @@
+package es.rmanzanodev.myfirstcomposeapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import es.rmanzanodev.myfirstcomposeapp.login.Greeting
+import es.rmanzanodev.myfirstcomposeapp.ui.theme.MyFirstComposeAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyFirstComposeAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Golazo espectacular",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyFirstComposeAppTheme {
+        Greeting("Android")
+    }
+}
