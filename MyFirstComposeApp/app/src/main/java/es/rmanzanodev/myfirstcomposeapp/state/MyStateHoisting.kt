ff14a0478508d0a76cc307061027f6b8bfcb0187
RafaelManzanoDev <manzanomedinarafael@gmail.com>
2025-07-12 14:30:03 +0200

    Seccion 3 - State hoisting

---
+++ MyFirstComposeApp/app/src/main/java/es/rmanzanodev/myfirstcomposeapp/state/MyStateHoisting.kt
@@ -0,0 +1,34 @@
+package es.rmanzanodev.myfirstcomposeapp.state
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+
+@Composable
+fun MyStateHoisting(modifier: Modifier) {
+    var number by rememberSaveable { mutableIntStateOf(0) }
+    Column(modifier = modifier) {
+        StateExample1(number = number, onClick = { number += 1 })
+        StateExample2(number = number, onClick = { number += 1 })
+    }
+}
+
+@Composable
+fun StateExample1(number: Int, onClick: () -> Unit) {
+    Text(text = "Pulsame $number", modifier = Modifier.clickable {
+        onClick()
+    })
+}
+
+@Composable
+fun StateExample2(number: Int, onClick: () -> Unit) {
+    Text(text = "Pulsame $number", modifier = Modifier.clickable {
+        onClick()
+    })
+}
